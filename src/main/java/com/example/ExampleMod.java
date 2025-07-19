939acb117787be224ea2f450b4e036e7c782e29e
URGG <91295255+URGG@users.noreply.github.com>
2025-07-18 23:07:27 -0700

    s

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public class ExampleMod implements ModInitializer {
 	public static final String MOD_ID = "modid";
 
-	// This logger is used to write text to the console and the log file.
+	// This logger is used to write text to the console and the log file.where os 
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
